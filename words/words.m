43ff093c33bf60e19bee382b951a5e6e4d1ada3d
Robert Vollmert <rvollmert@gmx.net>
2014-01-23 21:18:03 +0100

    show the solutions from words.m

--- words/words.m
+++ words/words.m
@@ -13,6 +13,7 @@
 :- import_module util.
 :- import_module examples.
 :- import_module solve.
+:- import_module grid.
 
 main(!IO) :-
       S = examples.size_1,
@@ -22,4 +23,17 @@ main(!IO) :-
                 Gs),
       io.write_string("found ", !IO),
       io.write(list.length(Gs) : int, !IO),
-      io.write_string(" solutions\n", !IO).
+      io.write_string(" solutions\n", !IO),
+      show_sols(Gs, !IO).
+
+:- pred show_sols(list(grid)::in, io::di, io::uo) is det.
+show_sols([], !IO).
+show_sols([G|Gs], !IO) :- write_lines(show(G), !IO),
+                          io.write_string("\n", !IO),
+                          show_sols(Gs, !IO).
+
+:- pred write_lines(list(string)::in, io::di, io::uo) is det.
+write_lines([], !IO).
+write_lines([L|Ls], !IO) :- io.write_string(L, !IO),
+                            io.write_string("\n", !IO),
+                            write_lines(Ls, !IO).
