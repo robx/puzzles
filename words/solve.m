f7da2f203b7d2891fb5f8618b22a494e48b69457
Robert Vollmert <rvollmert@gmx.net>
2014-01-24 00:03:25 +0100

    use the hgrid to enforce hints in solver

--- words/solve.m
+++ words/solve.m
@@ -10,21 +10,21 @@
 
 :- type hint == {point, char}.
 
-:- pred solve(list(word)::in, list(hint)::in, size::in, mgrid::out) is nondet.
+:- pred solve(list(word)::in, list(hint)::in, size::in, hgrid::out) is nondet.
 
 :- implementation.
 
 solve(Ws, [H | Hs], S, Gout) :-
     {P0, C} = H,
-    m_init(S, G),
-    place_chars(Hs, G, G1),
+    h_init(S, Hs, G),
     pick_cont(Ws, C, W0, Ws1),
-    place_word_char_any(P0, W0, C, G1, G2, Ps, Pe),
-    solve1(Ws1, Ps, Pe, G2, Gout).
-    % TODO: check that all hints are satisfied.
+    place_word_char_any(P0, W0, C, G, G1, Ps, Pe),
+    solve1(Ws1, Ps, Pe, G1, Gout),
+    h_no_hints(Gout).
     % TODO: check that no more than two words connect at any point
 
-:- pred solve1(list(word)::in, point::in, point::in, mgrid::in, mgrid::out) is nondet.
+:- pred solve1(list(word)::in, point::in, point::in, T::in, T::out)
+                                         is nondet <= (grid(T)).
 solve1([], Ps, Pe, G, Gout) :- Pe = Ps,
                                Gout = G.
 solve1(Ws, Ps, Pe, G, Gout) :-
