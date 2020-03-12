61ba444bffff9da73692b319e12f0a201a574293
Robert Vollmert <rob@vllmrt.net>
2020-03-12 18:59:00 +0100

    add some tests related to Val4 -1 enum crash

--- towerdefence/test/Main.hs
+++ towerdefence/test/Main.hs
@@ -3,7 +3,11 @@
 module Main where
 
 import Data.Holmes
+import Data.JoinSemilattice.Intersect (fromList, singleton)
+import Data.Propagator (lift)
 import Test.Hspec (describe, hspec, it, shouldBe)
+import Tower
+import TowerIntersect
 
 main :: IO ()
 main = hspec do
@@ -13,3 +17,14 @@ main = hspec do
       let prop _ = true
       result <- cfg `satisfying` prop
       result `shouldBe` Just [Exactly 1]
+    it "should be happy with 1 >= 1" do
+      let cfg = 1 `from` [1] :: Config Holmes (Intersect Val4)
+          one = 1 :: Val4
+          prop _ = lift (singleton one) .>= lift (singleton one)
+      result <- cfg `satisfying` prop
+      result `shouldBe` Just [fromList [one]]
+    it "shouldn't cause an exception" do
+      let cfg = 1 `from` [1 .. 4] :: Config Holmes (Intersect Val4)
+          prop _ = lift (singleton 0) .>= lift (singleton (1 :: Val4))
+      result <- cfg `satisfying` prop
+      result `shouldBe` Nothing
